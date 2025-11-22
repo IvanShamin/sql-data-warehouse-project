0f33f77f4bce330cea27196c4081ac8372151892
IvanShamin <vansoshamin@gmail.com>
2025-11-22 22:11:58 +0100

    Update proc_load_silver.sql

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -120,11 +120,15 @@ BEGIN
         PRINT '>> Inserting Data Into: silver.erp_cust_az12';
         INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
         SELECT
-            CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LEN(cid)) ELSE cid END,
-            CASE WHEN bdate > GETDATE() THEN NULL ELSE bdate END,
-            CASE WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
-                 WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
-                 ELSE 'n/a' END
+            CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LEN(cid)) ELSE cid END AS cid,
+            CASE WHEN bdate > GETDATE() THEN NULL ELSE bdate END AS bdate,
+        CASE 
+            WHEN REPLACE(REPLACE(UPPER(gen), CHAR(13), ''), CHAR(10), '') 
+                 LIKE '%FEMAL%' THEN 'Female'
+            WHEN REPLACE(REPLACE(UPPER(gen), CHAR(13), ''), CHAR(10), '') 
+                 LIKE '%MAL%'   THEN 'Male'
+            ELSE 'n/a'
+        END AS gen
         FROM bronze.erp_cust_az12;
 
         SET @Rows = @@ROWCOUNT;
